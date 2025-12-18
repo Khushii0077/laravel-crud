e72999ce510ceb0762cc31de0783d1b9e9b86273
Khushii0077 <khushimabian@gmail.com>
2025-12-18 20:52:40 +0530

    Added skills table, seeder, and dynamic APIs for dropdowns

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
             GenderSeeder::class,
             SubjectSeeder::class,
             StudentSeeder::class,
+            SkillSeeder::class
         ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+
 use App\Http\Controllers\StudentController;
+use App\Models\Gender;
+use App\Models\Course;
+use App\Models\Skill;
+
 
 Route::apiResource('students', StudentController::class);
 
+Route::get('/genders', function () {
+    return Gender::select(
+        'id',
+        DB::raw("CASE WHEN is_male = 1 THEN 'Male' ELSE 'Female' END as name")
+    )->get();
+});
 
+
+Route::get('/courses', function () {
+    return Course::select('id', 'course_name')->get();
+});
+
+Route::get('/skills', function () {
+    return Skill::select('id', 'name')->get();
+});
